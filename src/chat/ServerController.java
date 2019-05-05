363d99188702da27ad1b5ee20b2bfeb022f6ccd7
mansgrundberg <47752218+mansgrundberg@users.noreply.github.com>
2019-05-05 17:36:53 +0200

    Update ServerController.java

--- src/chat/ServerController.java
+++ src/chat/ServerController.java
@@ -1,184 +1,172 @@
 package chat;
 
-import java.awt.BorderLayout;
-import java.awt.CardLayout;
-import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Font;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.JScrollPane;
-import javax.swing.JTextArea;
-import javax.swing.SwingUtilities;
-
-/*
- * TEMPORÄR KLASS. ENDAST FÖR ATT TESTA CHATTSYSTEMET.
- * Måns Grundberg
+/**
+ * Handles the logic of the chat/login systems server side
+ * 
+ * @author Mans
+ *
  */
 
-@SuppressWarnings("serial")
-public class ChatTestUI extends JPanel implements ActionListener, KeyListener {
-	private JTextArea messages = new JTextArea();
-	private JTextArea newMessage = new JTextArea();
-	private JButton sendButton = new JButton("Send Message");
-	private JButton btnSwitch = new JButton("Show online users");
-	private JTextArea onlineUsers = new JTextArea();
-	private CardLayout card = new CardLayout();
-	private JPanel cardPanel;
-	private boolean showingMessages = true;
-	private ChatController controller;
+public class ServerController {
+	private ClientStreams clientStreams = new ClientStreams();
+	private RegisteredUsers users;
+	private ChatServer server;
 
-	public ChatTestUI (ChatController controller) {
-		this.controller = controller;
-		init();
-		setPanelInFrame(this);
+	public ServerController() {
+		loadUsers();
+	}
+	
+	public void addServer(ChatServer server) {
+		this.server = server;
 	}
 
-	private void init() {
-		setPreferredSize(new Dimension(300, 600));
-		setLayout(new BorderLayout());
-		setBackground(Color.BLACK);
-
-		sendButton.setBackground(Color.BLACK);
-		sendButton.setForeground(Color.BLACK);
-
-		JScrollPane scrollMessages = new JScrollPane(messages);
-		JScrollPane scrollPane2 = new JScrollPane(newMessage);
-		JPanel southPanel = new JPanel(new GridLayout(3, 0));
-		cardPanel = new JPanel(card);
-		JScrollPane scrollUsers = new JScrollPane(onlineUsers);
-
-		scrollMessages.setBackground(Color.BLACK);
-		scrollMessages.setForeground(Color.GREEN);
-		scrollPane2.setBackground(Color.BLACK);
-		scrollPane2.setForeground(Color.GREEN);
-		scrollUsers.setBackground(Color.BLACK);
-		southPanel.setBackground(Color.BLACK);
-
-		messages.setEditable(false);
-		onlineUsers.setEditable(false);
-		newMessage.setForeground(Color.GREEN);
-		newMessage.setBackground(Color.BLACK);
-		messages.setBackground(Color.BLACK);
-		messages.setForeground(Color.GREEN);
-		onlineUsers.setBackground(Color.BLACK);
-		onlineUsers.setForeground(Color.GREEN);
-
-		newMessage.addKeyListener(this);
-		messages.addKeyListener(this);
-
-		scrollMessages.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		scrollMessages.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollPane2.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		scrollPane2.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-		scrollUsers.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
-		scrollUsers.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
-
-		messages.setLineWrap(true);
-		messages.setWrapStyleWord(true);
-		newMessage.setLineWrap(true);
-		newMessage.setWrapStyleWord(true);
-		onlineUsers.setLineWrap(true);
-		onlineUsers.setWrapStyleWord(true);
-
-		newMessage.setCaretColor(Color.GREEN);
-
-		southPanel.setPreferredSize(new Dimension(300, 100));
-		cardPanel.add(scrollMessages);
-		cardPanel.add(scrollUsers);
-		southPanel.add(scrollPane2);
-		southPanel.add(sendButton);
-		southPanel.add(btnSwitch);
-		add(southPanel, BorderLayout.SOUTH);
-
-		add(cardPanel, BorderLayout.CENTER);
-
-		btnSwitch.addActionListener(this);
-		sendButton.addActionListener(this);
-		sendButton.setFont(new Font("MONOSPACE", Font.BOLD, 14));
-		btnSwitch.setFont(new Font("MONOSPACE", Font.BOLD, 14));
-
-		addMessage("Type @username to send a private message");
-		addMessage("Example: @Aragorn This is a message");
-	}
-
-	public void addMessage(String message) {
-		messages.append(message + "\n");
-	}
-
-	// TODO
-	public void updateUserList(UserList userList) {
-		onlineUsers.setText("");
-		onlineUsers.append("USERS ONLINE:" + "\n");
-		for (int i = 0; i < userList.size(); i++) {
-			onlineUsers.append(userList.get(i).getUsername() + "\n");
-		}
-		repaint();
-	}
-
-	@Override
-	public void actionPerformed(ActionEvent e) {
-		if (e.getSource() == sendButton && !newMessage.getText().isEmpty()) {
-			controller.sendMessage(newMessage.getText().trim());
-			newMessage.setText(null);
-			newMessage.setCaretPosition(0);
-		} else if (e.getSource() == btnSwitch) {
-			if (showingMessages) {
-				card.next(cardPanel);
-				btnSwitch.setText("Show messages");
-				showingMessages = false;
-				revalidate();
-			} else if (!showingMessages) {
-				card.first(cardPanel);
-				btnSwitch.setText("Show online users");
-				showingMessages = true;
-				revalidate();
+	/**
+	 * Sends the incoming message to all connected users or, if a private message,
+	 * to the specified receiver
+	 * 
+	 * @param message
+	 */
+	public void newMessage(Message message) {
+		UserList temp = new UserList(clientStreams.getKeySet());
+		if (message.getReceiver() == null) { // Not a private message, send to all
+			for (int i = 0; i < temp.size(); i++) {
+				if (!temp.get(i).getUsername().equals(message.getSender())) { // Exclude sender
+					server.sendObject(message, clientStreams.getOutputStream(temp.get(i)));
+				}
 			}
+		} else {
+			server.sendObject(message, clientStreams.getOutputStream(message.getReceiver()));
 		}
 	}
 
-	private void setPanelInFrame(JPanel panel) {
-		SwingUtilities.invokeLater(new Runnable() {
-			public void run() {
-				JFrame frame = new JFrame("Chatt");
-				frame.add(panel);
-				frame.pack();
-				frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-				frame.setVisible(true);
-				frame.setLocation(0, 0);
-				frame.setResizable(false);
-				frame.getRootPane().setDefaultButton(sendButton);
+	/**
+	 * Distributes an updated list of connected users
+	 * 
+	 * @param user
+	 */
+	private void sendUserList(User user) {
+		UserList temp = new UserList(clientStreams.getKeySet());
+		for (int i = 0; i < temp.size(); i++) {
+			server.sendObject(temp, clientStreams.getOutputStream(temp.get(i)));
+		}
+		newMessage(new Message(user.getUsername() + " is now online"));
+	}
+
+	/**
+	 * Checks whether user tries to login or create a new user
+	 * 
+	 * @param command  The command sent by the client, indicating the users desired
+	 *                 action
+	 * @param user     The specified user
+	 * @param password The specified password
+	 * @param oos      The ObjectOutputStream connected to the Client in question
+	 * @return True if login/creating user is successful, otherwise false
+	 */
+
+	public boolean login(String command, User user, String password, ObjectOutputStream oos) {
+		if (command.equals("LOGIN")) {
+			if (clientStreams.contains(user)) {
+				server.sendObject("User already logged in", oos);
+				return false;
+			} else {
+				System.out.println("Kollar lösenord");
+				return checkPassword(user, password, oos);
 			}
-		});
-	}
-
-	@Override
-	public void keyTyped(KeyEvent e) {
-	}
-
-	@Override
-	public void keyPressed(KeyEvent e) {
-		if (e.getKeyCode() == KeyEvent.VK_ENTER) {
-			sendButton.doClick();
-			e.consume();
-		} else if (e.getKeyCode() == KeyEvent.VK_TAB) {
-			btnSwitch.doClick();
-			e.consume();
+		} else {
+			return newUser(user, password, oos);
 		}
 	}
 
-	@Override
-	public void keyReleased(KeyEvent e) {
+	/**
+	 * Checks whether the specified password matches the stores password for the
+	 * user in question
+	 * 
+	 * @param user     The specified user
+	 * @param password The specified password
+	 * @param oos      The ObjectOutputStream connected to the Client in question
+	 * @return True if password is correct, otherwise false
+	 */
+
+	private boolean checkPassword(User user, String password, ObjectOutputStream oos) {
+		if (users.contains(user) && users.checkPassword(user, password)) {
+			clientStreams.put(user, oos);
+			server.sendObject(user, oos);
+			server.sendObject("LOGIN OK", oos);
+			sendUserList(user);
+			return true;
+		} else {
+			server.sendObject("Username and/or password is incorrect", oos);
+			return false;
+		}
 	}
 
-	public static void main(String[] args) {
-		new ChatTestUI(new ChatController());
+	/**
+	 * Checks whether username is already in use. If not, adds user to list of
+	 * registered users
+	 * 
+	 * @param user     The user to be created
+	 * @param password The specified password
+	 * @param oos      The ObjectOutputStream connected to the Client in question
+	 * @return True if user is successfully created, false if username is already in
+	 *         use
+	 */
+
+	private boolean newUser(User user, String password, ObjectOutputStream oos) {
+		if (users.contains(user)) {
+			server.sendObject("Username already in use", oos);
+			return false;
+		} else {
+			users.put(user, password);
+			clientStreams.put(user, oos);
+			server.sendObject(user, oos);
+			server.sendObject("USER CREATED", oos);
+			sendUserList(user);
+			saveUsers();
+			return true;
+		}
 	}
+
+	/**
+	 * Removes the user and it's corresponding ObjectOutputStream from list
+	 * 
+	 * @param user The disconnecting user
+	 */
+	public void disconnectUser(User user) {
+		clientStreams.remove(user);
+	}
+
+	/**
+	 * Reads list of registered users from file
+	 */
+	private void loadUsers() {
+		File contactFile = new File("files/registeredUsers.dat");
+		if (contactFile.exists()) {
+			try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(contactFile))) {
+				users = (RegisteredUsers) ois.readObject();
+			} catch (IOException | ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+		} if (users == null) {
+			users = new RegisteredUsers();
+		}
+	}
+
+	/**
+	 *  Writes list registered users to file
+	 */
+	private void saveUsers() {
+		try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream("files/registeredUsers.dat"))) {
+			oos.writeObject(users);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
 }
