7d6c3f565a69f0d6a37371e7df4d301b0eb7bec0
spektrum1 <44115155+spektrum1@users.noreply.github.com>
2019-05-15 16:07:04 +0200

    konflikt

--- src/chat/ChatController.java
+++ src/chat/ChatController.java
@@ -23,7 +23,7 @@ public class ChatController {
 
 
 	public ChatController() {
-		client = new ChatClient(60000, "10.2.14.16", this);
+		client = new ChatClient(60000, "localhost", this);
 		client.connect();
 		initLoginUI();
 		chatUI = new ChatUI(this);
