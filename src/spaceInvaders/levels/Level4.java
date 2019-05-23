2c918fa6e714e5b5f1b86560fdeb12956f72f15d
spektrum1 <44115155+spektrum1@users.noreply.github.com>
2019-05-23 21:45:04 +0200

    nya levels

--- src/spaceInvaders/levels/Level4.java
+++ src/spaceInvaders/levels/Level4.java
@@ -6,15 +6,15 @@ import spaceInvaders.units.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class Level3 extends Level {
+public class Level4  extends Level {
 
     private static final List<List<Enemy>> enemyBlock = new ArrayList<>();
     static {
         for (int i  = 0 ; i < 4; i ++) {
             enemyBlock.add(new ArrayList<>());
             for (int j = 0; j < 8; j++ ) {
-                if ( i % 2 == 1)
-                enemyBlock.get(i).add(new Level3Enemy(new Position((200+ j*50),35+(i*30)),null,Difficulty.EASY));
+                if (j < 4)
+                    enemyBlock.get(i).add(new Level3Enemy(new Position((200+ j*50),35+(i*30)),null,Difficulty.EASY));
                 else {
                     enemyBlock.get(i).add(new Level4Enemy(new Position((200+ j*50),35+(i*30)),null,Difficulty.EASY));
 
@@ -23,7 +23,7 @@ public class Level3 extends Level {
         }
     }
 
-    public Level3(Difficulty difficulty, Controller controller) {
+    public Level4(Difficulty difficulty, Controller controller) {
         super(difficulty, enemyBlock, controller);
     }
 
